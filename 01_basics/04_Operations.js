d2c4c186380f7a0ffc654d8cfe0dada3f54f6b90
apoorvraj6 <128055382+apoorvraj6@users.noreply.github.com>
2024-05-25 14:04:06 +0000

    operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,36 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+
+let str1 = "Apoorv"
+let str2 = "Raj"
+console.log(str1 + " " + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2) //first value is string so all will be converted to string
+// console.log(2 + 2 + "1") // first value is number so operation will be performed on number and then string is added 
+
+
+// console.log(+true) //converted to number
+
+// console.log(+"");
+
+// let num1,num2,num3;
+// num1 = num2 = num3 = 2+2; 
+
+// let gamecounter=100;
+// // ++gamecounter;
+// console.log(gamecounter++)
+// console.log(gamecounter); //prefix and postfix operator
+
+ 
+
