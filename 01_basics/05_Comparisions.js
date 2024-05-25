c87a3d99ce6f2687e4687d43a5d2f6fcba80c93e
apoorvraj6 <128055382+apoorvraj6@users.noreply.github.com>
2024-05-25 14:15:05 +0000

    comparisions

---
+++ 01_basics/05_Comparisions.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2<1)
+// console.log(2==1)
+
+// console.log("2" > 1);
+// console.log("02" > 1); //Js allow to compare two data types
+
+// console.log(null > 0) // the value of null depends upon the comparision operator
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0)
+// console.log(undefined>=0)
+// console.log(undefined<0)
+
+// strict check === check value as well as datatype
+
+console.log("2"==2)
+console.log("2" === 2)
+console.log("2" === "3")
+console.log("2"==="2")
