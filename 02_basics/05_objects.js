6b65b0f39aa3e0ab39ae216f746fb38b06b89c12
apoorvraj6 <128055382+apoorvraj6@users.noreply.github.com>
2024-05-28 11:43:19 +0000

    json talk

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,29 @@
+const course = {
+    name:"js",
+    price:"999",
+    teacher:"chai bhai",
+}
+
+// console.log(course.teacher);
+
+const {teacher : t} = course //destructe of object
+// console.log(teacher);
+console.log(t);
+
+// <-------------------------------API-------------------------->
+
+// {
+//     "name" :"apoorv",
+//     "course":"js in hindi",
+//     "price":"free"
+// }
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
+//json file is a file which is used to store the data from an API
+
+// json formatter is a tool to study a json file
