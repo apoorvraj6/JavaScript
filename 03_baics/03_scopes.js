599a54f711e83d9ef31204aacc91b2b6d5adf1a5
apoorvraj6 <128055382+apoorvraj6@users.noreply.github.com>
2024-05-30 13:02:51 +0000

    block scope

---
+++ 03_baics/03_scopes.js
@@ -0,0 +1,19 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+ let a=300;
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30;
+    console.log("inner: ",a);
+}
+// console.log(a); //a not present
+// console.log(b); // b not present
+console.log(c);
+console.log(a);
+
+
+
