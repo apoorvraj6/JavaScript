abd10de254f28893b00ef4a2e422803d45a04d60
apoorvraj6 <128055382+apoorvraj6@users.noreply.github.com>
2024-06-01 06:02:45 +0000

    while and do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,18 @@
+// while (condition) {
+
+// }
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+// do {
+    
+// } while (condition);
+let j=0;
+do {
+    console.log(`result of do while ${j}`);
+    j++;
+} while (j>1) // Execute atleast one time.
